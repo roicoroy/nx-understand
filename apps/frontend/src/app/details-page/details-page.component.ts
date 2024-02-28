02178630a3501011bf3940e56013dd4d9e094564
Ricardo Watanabe Bento <ricardo.bento@ext.codere.com>
2024-02-28 09:32:53 +0000

    lax

--- apps/frontend/src/app/details-page/details-page.component.ts
+++ apps/frontend/src/app/details-page/details-page.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonTitle, IonToolbar, IonHeader, IonButtons, IonBackButton } from "@ionic/angular/standalone";
-
+import { IonTitle, IonToolbar, IonHeader, IonButtons, IonBackButton, IonContent, IonLabel, IonItem } from "@ionic/angular/standalone";
+declare var lax: any;
 @Component({
   selector: 'nx-understand-details-page',
   standalone: true,
-  imports: [IonBackButton, IonButtons, IonHeader, IonToolbar, IonTitle, CommonModule],
+  imports: [IonItem, IonLabel, IonContent, IonBackButton, IonButtons, IonHeader, IonToolbar, IonTitle, CommonModule],
   templateUrl: './details-page.component.html',
   styleUrl: './details-page.component.css',
 })
-export class DetailsPageComponent {}
+export class DetailsPageComponent implements AfterViewInit, OnInit {
+
+  ngAfterViewInit() {
+    window.onload = function () {
+      lax.init()
+
+      const input: any = document.getElementById('input')
+
+      // Add lax driver for inputLength
+      lax.addDriver('inputLength', function () {
+        return input.value.length
+      })
+
+      lax.addElements("#input", {
+        'inputLength': {
+          "rotate": [
+            [0, 100],
+            [0, 360],
+          ],
+        }
+      })
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
+}
